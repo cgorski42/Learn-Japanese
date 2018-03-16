fefb3b7881b7f2b36376ff451e541edc466919a3
nightwingxoracle <cgorski42@gmail.com>
2018-03-16 14:05:47 -0600

    small server changes

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ let q_mode = 2;
 
 app.get('/api/items/', (req, res) => {
     res.send({ans_mode:ans_mode, q_mode:q_mode});
-}
+});
 
 app.post('/api/items/', (req, res) => {
     ans_mode = req.body.ans_mode;
     q_mode = req.body.q_mode;
     res.send({ans_mode:ans_mode, q_mode:q_mode});
-}
+});
 
-app.listen(3000, () => console.log('Server listening on port 3000!'))
+app.listen(3000, () => console.log('Server listening on port 3000!'));
 
-app.put('/api/items/:id', (req, res) => {}
+app.put('/api/items/:id', (req, res) => {});
 
-app.delete('/api/items/:id', (req, res) => {}
+app.delete('/api/items/:id', (req, res) => {});
